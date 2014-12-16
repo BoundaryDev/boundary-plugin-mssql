c3fa3180e41020a918e11f16ae07114d4f0e51a9
Nitzan Miron <bug.assembla@bugbug.me>
2014-12-15 17:18:41 -0800

    Added scaling for percentages.

--- plugin.ps1
+++ plugin.ps1
@@ -1,18 +1,18 @@
 $METRICS = @(
-    @("\SQLServer:General Statistics\Active Temp Tables", "MSSQL_ACTIVE_TEMP_TABLES"),
-    @("\SQLServer:General Statistics\User Connections", "MSSQL_USER_CONNECTIONS"),
-    @("\SQLServer:General Statistics\Logical Connections", "MSSQL_LOGICAL_CONNECTIONS"),
-    @("\SQLServer:General Statistics\Transactions", "MSSQL_TRANSACTIONS"),
-    @("\SQLServer:General Statistics\Processes blocked", "MSSQL_PROCESSES_BLOCKED"),
-    @("\SQLServer:Locks(_total)\Lock Timeouts/sec", "MSSQL_LOCK_TIMEOUTS"),
-    @("\SQLServer:Locks(_total)\Lock Waits/sec", "MSSQL_LOCK_WAITS"),
-    @("\SQLServer:Locks(_total)\Lock Wait Time (ms)", "MSSQL_LOCK_WAIT_TIME_MS"),
-    @("\SQLServer:Locks(_total)\Average Wait Time (ms)", "MSSQL_LOCK_AVERAGE_WAIT_TIME_MS"),
-    @("\SQLServer:Locks(_total)\Lock Timeouts (timeout > 0)/sec", "MSSQL_LOCK_TIMEOUTS_GT0"),
-    @("\SQLServer:Databases(_total)\Percent Log Used", "MSSQL_PERCENT_LOG_USED"),
-    @("\SQLServer:Databases(_total)\Repl. Pending Xacts", "MSSQL_REPL_PENDING_XACTS"),
-    @("\SQLServer:SQL Statistics\SQL Compilations/sec", "MSSQL_COMPILATIONS"),
-    @("\SQLServer:Wait Statistics(_total)\Page IO latch waits", "MSSQL_PAGE_IO_LATCH_WAITS")
+    @("\SQLServer:General Statistics\Active Temp Tables", "MSSQL_ACTIVE_TEMP_TABLES", 1),
+    @("\SQLServer:General Statistics\User Connections", "MSSQL_USER_CONNECTIONS", 1),
+    @("\SQLServer:General Statistics\Logical Connections", "MSSQL_LOGICAL_CONNECTIONS", 1),
+    @("\SQLServer:General Statistics\Transactions", "MSSQL_TRANSACTIONS", 1),
+    @("\SQLServer:General Statistics\Processes blocked", "MSSQL_PROCESSES_BLOCKED", 1),
+    @("\SQLServer:Locks(_total)\Lock Timeouts/sec", "MSSQL_LOCK_TIMEOUTS", 1),
+    @("\SQLServer:Locks(_total)\Lock Waits/sec", "MSSQL_LOCK_WAITS", 1),
+    @("\SQLServer:Locks(_total)\Lock Wait Time (ms)", "MSSQL_LOCK_WAIT_TIME_MS", 1),
+    @("\SQLServer:Locks(_total)\Average Wait Time (ms)", "MSSQL_LOCK_AVERAGE_WAIT_TIME_MS", 1),
+    @("\SQLServer:Locks(_total)\Lock Timeouts (timeout > 0)/sec", "MSSQL_LOCK_TIMEOUTS_GT0", 1),
+    @("\SQLServer:Databases(_total)\Percent Log Used", "MSSQL_PERCENT_LOG_USED", 0.01),
+    @("\SQLServer:Databases(_total)\Repl. Pending Xacts", "MSSQL_REPL_PENDING_XACTS", 1),
+    @("\SQLServer:SQL Statistics\SQL Compilations/sec", "MSSQL_COMPILATIONS", 1),
+    @("\SQLServer:Wait Statistics(_total)\Page IO latch waits", "MSSQL_PAGE_IO_LATCH_WAITS", 1)
 )
 
 $METRIC_COUNTERS = @()
@@ -24,13 +24,14 @@ foreach ($metric_info in $METRICS)
 {
     $counter_name = $metric_info[0]
     $boundary_name = $metric_info[1]
+    $scale = $metric_info[2]
     
     try
     {
 	   $data = Get-Counter $counter_name -ErrorAction Stop
        # Write-Host ("{0} -> {1}" -f $data.CounterSamples[0].Path, $boundary_name)
        $METRIC_COUNTERS += $counter_name
-       $BOUNDARY_NAME_MAP[$data.CounterSamples[0].Path] = $boundary_name
+       $BOUNDARY_NAME_MAP[$data.CounterSamples[0].Path] = $boundary_name, $scale
     }
     catch
     {
@@ -45,7 +46,10 @@ function outputMetrics
     
     foreach ($item in $data.counterSamples)
     {
-        Write-Host ("{0} {1}" -f $BOUNDARY_NAME_MAP[$item.Path], $item.CookedValue)
+        Write-Host $item
+        $boundary_name = $BOUNDARY_NAME_MAP[$item.Path][0]
+        $value = $item.CookedValue * $BOUNDARY_NAME_MAP[$item.Path][1]
+        Write-Host ("{0} {1}" -f $boundary_name, $value)
     }
 }
 
